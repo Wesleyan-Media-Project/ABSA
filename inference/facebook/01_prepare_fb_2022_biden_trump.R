ae0411a55061603f058ef5345be0b99de47c655e
Markus Neumann <mneumann.polsci@gmail.com>
2023-01-11 22:15:04 -0500

    2022 ABSA

---
+++ inference/facebook/01_prepare_fb_2022_biden_trump.R
@@ -0,0 +1,35 @@
+# Create the inference dataset
+
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+# Input paths
+path_intermediary_1 <- "../../../entity_linking_2022/facebook/data/entity_linking_results_fb22.csv.gz"
+# Output paths
+path_prepared_for_absa <- "../../data/118m_prepared_for_ABSA.csv"
+
+df <- fread(path_intermediary_1, encoding = "UTF-8")
+
+# Transform the Python-based detected entities field so it can be split later
+df <- df %>% mutate(across(c(text_detected_entities, text_start, text_end), str_remove_all, "\\[|\\]|\\'"))
+df <- df %>% mutate(across(c(text_detected_entities, text_start, text_end), str_split, ", "))
+
+# Remove all ads with no detected entities
+df <- df %>% filter(text_detected_entities != "")
+# Split the Python-based id field
+df$id <- str_split(df$id, "\\|")
+
+# Unnest multiple detected entities
+df <- unnest(df, cols = c(text_detected_entities, text_start, text_end))
+
+df$text_start <- as.numeric(df$text_start)
+df$text_end <- as.numeric(df$text_end)
+
+df$chunk1 <- substr(df$text, 1, df$text_start)
+df$chunk2 <- substr(df$text, df$text_end+1, nchar(df$text))
+df$text <- paste0(df$chunk1, "$T$", df$chunk2)
+df <- select(df, -c(chunk1, chunk2))
+
+fwrite(df, "../../data/fb2022_prepared_for_ABSA.csv")
