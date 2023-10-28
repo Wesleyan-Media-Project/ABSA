cc8cc4cec11b017d4b578a8fcfe6d8fd76c81e59
furkmak <93638016+furkmak@users.noreply.github.com>
2023-10-28 05:49:22 -0400

    Add files via upload

---
+++ inference/google/01_prepare_google_2022.R
@@ -0,0 +1,54 @@
+# Create the inference dataset
+
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(R.utils)
+library(tidyverse)
+
+# Input paths
+path_intermediary_1 <- "../entity_linking/google/data/entity_linking_results_google_2022_20231028.csv.gz"
+# Output paths
+path_prepared_for_absa <- "../data/google2022_prepared_for_ABSA.csv"
+
+
+
+df <- fread(path_intermediary_1, encoding = "UTF-8", data.table = F)
+
+df$detected_entities <- df$text_detected_entities
+df$start <- df$text_start
+df$end <- df$text_end
+
+df <- df %>% select(-c(text_detected_entities, text_start, text_end))
+
+df2 <- df[df$detected_entities != "[]",]
+
+# Extract the character indices
+python_to_r_list <- function(x){
+  
+  x %>%
+    str_remove_all(" ") %>%
+    str_remove_all("\\[") %>%
+    str_remove_all("\\]") %>%
+    str_split(",")
+  
+}
+
+df2$start <- python_to_r_list(df2$start)
+df2$end <- python_to_r_list(df2$end)
+df2$detected_entities <- python_to_r_list(df2$detected_entities)
+
+# One row per detected entity
+df3 <- unnest(df2, c(detected_entities, start, end))
+df3$detected_entities <- str_remove_all(df3$detected_entities, "'")
+df3$start <- as.numeric(df3$start)
+df3$end <- as.numeric(df3$end)
+
+df3$chunk1 <- substr(df3$text, 1, df3$start)
+df3$chunk2 <- substr(df3$text, df3$end+1, nchar(df3$text))
+df3$text <- paste0(df3$chunk1, "$T$", df3$chunk2)
+df4 <- select(df3, -c(chunk1, chunk2))
+
+
+fwrite(df4, path_prepared_for_absa)
---
+++ inference/google/02_rf_google_2022.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
+from joblib import dump, load
+import pickle
+
+# Input data
+path_prepared_google_2022 = "./data/google2022_prepared_for_ABSA.csv"
+path_model = './train/models/trained_absa_rf_2022.joblib'
+# Output data
+path_predictions = './data/google_2022_ABSA_pred.csv.gz'
+
+# Inference on the Google 2020 dataset
+df_inf = pd.read_csv(path_prepared_google_2022)
+text_clf = load(path_model)
+predictedgoogle_2022 = text_clf.predict(df_inf['text'])
+
+df_inf['predicted_sentiment'] = predictedgoogle_2022
+
+# Save without text column
+df_inf = df_inf.drop(columns = 'text')
+df_inf.to_csv(path_predictions, index = False)
